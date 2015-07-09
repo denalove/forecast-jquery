f09a1240fddd8ce7b0f07665633dd813c7f46cab
Dena Sawyer <denasawyer1111@gmail.com>
2015-07-09 17:02:39 -0400

    Add toggle to day and change glyphicon

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 var main = function() {
   
+
+  $('.day').click(function() {
+  	$(this).next().toggle();
+
+  });
+
+  $('.day').click(function() {
+  	$('.weekday').find('span').toggleClass('glyphicon-plus  glyphicon-minus');
+  });
+
 };
 
 $(document).ready(main);
